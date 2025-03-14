7e9d63b2905eab2d7a3603bc5c7c5d1b646bb1d5
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-03-14 20:45:09 +0530

    implemented both methods of PagingSource library load and getRefreshKey

--- Android-Paging-3/app/src/main/java/com/imthiyas/paging3/paging/QuotePagingSource.kt
+++ Android-Paging-3/app/src/main/java/com/imthiyas/paging3/paging/QuotePagingSource.kt
@@ -6,11 +6,29 @@ import com.imthiyas.paging3.model.Result
 import com.imthiyas.paging3.retrofit.QuotesAPI
 
 class QuotePagingSource(val quotesAPI: QuotesAPI) : PagingSource<Int, Result>() {
-    override fun getRefreshKey(state: PagingState<Int, Result>): Int? {
-        TODO("Not yet implemented")
-    }
+
 
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Result> {
-        TODO("Not yet implemented")
+
+        try {
+            val pagePosition = params.key ?: 1
+            val quotesResponse = quotesAPI.getQuotes(pagePosition)
+            return LoadResult.Page(
+                data = quotesResponse.result,
+                prevKey = if (pagePosition == 1) null else pagePosition - 1,
+                nextKey = if (pagePosition == quotesResponse.totalPage) null else pagePosition + 1
+            )
+        } catch (e: Exception) {
+            return LoadResult.Error(e)
+        }
+
     }
+
+    override fun getRefreshKey(state: PagingState<Int, Result>): Int? {
+        return state.anchorPosition?.let {
+            state.closestPageToPosition(it)?.prevKey?.plus(1)
+                ?: state.closestPageToPosition(it)?.nextKey?.minus(1)
+        }
+    }
+
 }
---
+++ Android-Paging-3/app/src/main/java/com/imthiyas/paging3/repository/QuoteRepository.kt
@@ -0,0 +1,6 @@
+package com.imthiyas.paging3.repository
+
+import com.imthiyas.paging3.retrofit.QuotesAPI
+
+class QuoteRepository(private val quotesAPI: QuotesAPI) {
+}
