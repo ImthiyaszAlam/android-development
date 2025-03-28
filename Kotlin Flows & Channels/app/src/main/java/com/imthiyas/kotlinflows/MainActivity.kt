e127e9378f1bad926b23089489981044cfc38c76
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-03-28 23:35:07 +0530

    consumer and producer

--- Kotlin Flows & Channels/app/src/main/java/com/imthiyas/kotlinflows/MainActivity.kt
+++ Kotlin Flows & Channels/app/src/main/java/com/imthiyas/kotlinflows/MainActivity.kt
@@ -5,16 +5,28 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.internal.NopCollector.emit
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        GlobalScope.launch {
+            
+        }
+
+        fun producer():Flow<Int>{
+            val list  = listOf(1,2,3,4,5,6,7,8,9,10)
+            list.forEach {
+                delay(1000),
+                emit(it)
+            }
         }
     }
 }
