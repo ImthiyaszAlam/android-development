e22d89d9ddacf4eacca23be50ffe76ca09b2d795
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-03-30 22:41:39 +0530

    flow is interesting

--- Kotlin Flows & Channels/app/build.gradle.kts
+++ Kotlin Flows & Channels/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.imthiyas.kotlinflows"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.imthiyas.kotlinflows"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -38,6 +38,7 @@ android {
 dependencies {
 
     implementation(libs.androidx.core.ktx)
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
--- Kotlin Flows & Channels/app/src/main/java/com/imthiyas/kotlinflows/MainActivity.kt
+++ Kotlin Flows & Channels/app/src/main/java/com/imthiyas/kotlinflows/MainActivity.kt
@@ -1,32 +1,55 @@
 package com.imthiyas.kotlinflows
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.internal.NopCollector.emit
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "MainActivityDebug"
+    }
+
+
+    @OptIn(DelicateCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        GlobalScope.launch {
-            
+        GlobalScope.launch(Dispatchers.Main) {
+
+            producer()
+                .onStart {
+                    Log.d(TAG, "Starting out")
+                }
+                .onCompletion {
+                    Log.d(TAG, "Completed")
+                }
+                .onEach {
+                    Log.d(TAG, "About to emi $it")
+                }
+                .collect {
+                    Log.d(TAG, it.toString())
+                }
         }
 
-        fun producer():Flow<Int>{
-            val list  = listOf(1,2,3,4,5,6,7,8,9,10)
-            list.forEach {
-                delay(1000),
-                emit(it)
-            }
+    }
+
+
+    suspend fun producer(): Flow<Int> = flow<Int> {
+        val list = listOf(1, 2, 3, 4, 5, 6)
+        list.forEach {
+            delay(1000)
+            emit(it)
         }
     }
 }
