54cbd85125ca22da4e98dc89417418963a09c8b8
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-04-22 00:01:22 +0530

    FCM

---
+++ Push Notification FCM/app/src/main/java/com/imthiyas/fcm/FCMService.kt
@@ -0,0 +1,17 @@
+package com.imthiyas.fcm
+
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class FCMService : FirebaseMessagingService() {
+
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+    }
+
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+    }
+}
