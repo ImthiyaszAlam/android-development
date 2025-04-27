a660df7ff3f6c46f6a35764e578c5b3d22bb2b84
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-04-27 17:00:01 +0530

    retrofit instance creation

--- Push Notification FCM/app/src/main/java/com/imthiyas/fcm/ChatViewModel.kt
+++ Push Notification FCM/app/src/main/java/com/imthiyas/fcm/ChatViewModel.kt
@@ -2,10 +2,14 @@ package com.imthiyas.fcm
 
 import androidx.lifecycle.ViewModel
 import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
 
-class ChatViewModel:ViewModel() {
+class ChatViewModel : ViewModel() {
 
-    private val api:FCMApiService = Retrofit.Builder()
+    private val api: FCMApiService = Retrofit.Builder()
         .baseUrl("baseUrl")
-        .addConverterFactory(Mosh)
+        .addConverterFactory(MoshiConverterFactory.create())
+        .build()
+        .create()
 }
