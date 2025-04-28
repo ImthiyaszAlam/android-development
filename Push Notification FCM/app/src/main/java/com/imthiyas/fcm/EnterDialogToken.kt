5479b611611d147fe26a8aaa67c1dd8ae56d2767
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-04-29 00:29:29 +0530

    dialog creation and cancel dismissal

--- Push Notification FCM/app/src/main/java/com/imthiyas/fcm/EnterDialogToken.kt
+++ Push Notification FCM/app/src/main/java/com/imthiyas/fcm/EnterDialogToken.kt
@@ -1,7 +1,11 @@
 package com.imthiyas.fcm
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 
 @Composable
 fun EnterTokenDialog(
@@ -9,5 +13,18 @@ fun EnterTokenDialog(
     onTokenChange: (String) -> Unit,
     onSubmit: () -> Unit
 ) {
-    val cli
+    val clipboardManager = LocalClipboardManager.current
+    val context = LocalContext.current
+    val scope = rememberCoroutineScope()
+
+    Dialog(
+        onDismissRequest = {},
+        properties = DialogProperties(
+            dismissOnBackPress = false,
+            dismissOnClickOutside = false
+        )
+    ) {
+
+
+    }
 }
