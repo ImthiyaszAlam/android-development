0401ab754695d6aac4233f0d8ade96ee9ae27b89
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-04-29 00:33:24 +0530

    added modifier to beautify dialobox

--- Push Notification FCM/app/src/main/java/com/imthiyas/fcm/EnterDialogToken.kt
+++ Push Notification FCM/app/src/main/java/com/imthiyas/fcm/EnterDialogToken.kt
@@ -1,9 +1,18 @@
 package com.imthiyas.fcm
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 
@@ -25,6 +34,15 @@ fun EnterTokenDialog(
         )
     ) {
 
+        Column(
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(5.dp))
+                .background(MaterialTheme.colorScheme.surface)
+                .padding(16.dp)
+        ) {
 
+        }
     }
 }
