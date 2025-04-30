b7e2a9a167b6c5bd1f12b68d16929d7de7deec8a
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-04-30 21:37:51 +0530

    ChatScreen

---
+++ Push Notification FCM/app/src/main/java/com/imthiyas/fcm/ChatScreen.kt
@@ -0,0 +1,13 @@
+package com.imthiyas.fcm
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ChatScreen(
+    messageText: String,
+    onMessageChange: (String) -> Unit,
+    onMessageSend: () -> Unit,
+    onMessageBroadCast: () -> Unit
+) {
+
+}
