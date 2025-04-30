76ec0a1f923de03e590d9bae54aa8b051c70f736
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-04-30 21:42:51 +0530

    added modifier

--- Push Notification FCM/app/src/main/java/com/imthiyas/fcm/ChatScreen.kt
+++ Push Notification FCM/app/src/main/java/com/imthiyas/fcm/ChatScreen.kt
@@ -1,6 +1,16 @@
 package com.imthiyas.fcm
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun ChatScreen(
@@ -9,5 +19,22 @@ fun ChatScreen(
     onMessageSend: () -> Unit,
     onMessageBroadCast: () -> Unit
 ) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+
+    ) {
+        OutlinedTextField(
+            value = messageText,
+            onValueChange = onMessageChange,
+            placeholder = {
+                Text("Enter a Message")
+            },
+            modifier = Modifier
+                .padding(horizontal = 10.dp)
+                .fillMaxWidth()
+        )
+    }
 
 }
