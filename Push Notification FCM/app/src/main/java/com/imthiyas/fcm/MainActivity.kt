33066a5152a69c99f39e36b1203691f0f95d6b64
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-05-08 21:46:42 +0530

    added run time permission

--- Push Notification FCM/app/src/main/java/com/imthiyas/fcm/MainActivity.kt
+++ Push Notification FCM/app/src/main/java/com/imthiyas/fcm/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.imthiyas.fcm
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -7,13 +10,19 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        const val NOTIFICATION_REQ_CODE = 100
+    }
 
     private val viewModel: ChatViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,5 +56,26 @@ class MainActivity : AppCompatActivity() {
             }
 
         }
+    }
+
+
+    private fun requestPostPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            val hasPermission = ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) == PackageManager.PERMISSION_GRANTED
+
+            if (hasPermission) {
+                return
+            } else {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                    NOTIFICATION_REQ_CODE
+                )
+            }
+        }
+
     }
 }
