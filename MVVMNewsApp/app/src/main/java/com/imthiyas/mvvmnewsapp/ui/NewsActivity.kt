f18fd8eaeb098a2ebb46fed815100869ce045860
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-05-15 13:45:15 +0530

    animation

--- MVVMNewsApp/app/src/main/java/com/imthiyas/mvvmnewsapp/ui/NewsActivity.kt
+++ MVVMNewsApp/app/src/main/java/com/imthiyas/mvvmnewsapp/ui/NewsActivity.kt
@@ -5,6 +5,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.imthiyas.mvvmnewsapp.R
 
 class NewsActivity : AppCompatActivity() {
@@ -17,6 +22,9 @@ class NewsActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val navController = findNavController(R.id.newsNavHostFragment)
+        val bottomNav = findViewById<BottomNavigationView>(R.id.navigationView)
+        bottomNav.setupWithNavController(navController)
 
     }
 }
