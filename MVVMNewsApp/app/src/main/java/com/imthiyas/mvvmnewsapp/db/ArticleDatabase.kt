1a484be7e5795b395737bc70d8f95dd8f012a060
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-05-16 11:54:08 +0530

    room db creation and type converter

--- MVVMNewsApp/app/src/main/java/com/imthiyas/mvvmnewsapp/db/ArticleDatabase.kt
+++ MVVMNewsApp/app/src/main/java/com/imthiyas/mvvmnewsapp/db/ArticleDatabase.kt
@@ -1,7 +1,10 @@
 package com.imthiyas.mvvmnewsapp.db
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.imthiyas.mvvmnewsapp.Article
 
 
@@ -9,10 +12,25 @@ import com.imthiyas.mvvmnewsapp.Article
     entities = [Article::class],
     version = 1
 )
+
+@TypeConverters(TypeConverter::class)
 abstract class ArticleDatabase : RoomDatabase() {
     abstract fun getArticleDao(): ArticleDao
 
     companion object {
-        
+        @Volatile
+        private var instance: ArticleDatabase? = null
+        private val LOCK = Any()
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
+            instance ?: createDatabase(context).also { instance = it }
+        }
+
+        private fun createDatabase(context: Context) =
+            Room.databaseBuilder(
+                context.applicationContext,
+                ArticleDatabase::class.java,
+                "article_db.db"
+            )
+                .build()
     }
 }
---
+++ MVVMNewsApp/app/src/main/java/com/imthiyas/mvvmnewsapp/db/TypeConverter.kt
@@ -0,0 +1,16 @@
+package com.imthiyas.mvvmnewsapp.db
+
+import androidx.room.TypeConverter
+import com.imthiyas.mvvmnewsapp.Source
+
+class TypeConverter {
+    @TypeConverter
+    fun fromSource(source: Source): String {
+        return source.name
+    }
+
+    @TypeConverter
+    fun toSource(name: String): Source {
+        return Source(name, name)
+    }
+}
