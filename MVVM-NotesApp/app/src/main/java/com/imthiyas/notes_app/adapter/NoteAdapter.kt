fde6742dd476f43c8eac504365b1dde116c402ec
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-05-22 16:34:54 +0530

    3 callbacks onBindView,getItemCount and onCreateViewH

--- MVVM-NotesApp/app/src/main/java/com/imthiyas/notes_app/adapter/NoteAdapter.kt
+++ MVVM-NotesApp/app/src/main/java/com/imthiyas/notes_app/adapter/NoteAdapter.kt
@@ -1,10 +1,14 @@
 package com.imthiyas.notes_app.adapter
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.imthiyas.notes_app.databinding.NoteLayoutBinding
 import com.imthiyas.notes_app.model.Note
+import com.imthiyas.notes_app.ui.fragments.HomeFragmentDirections
 
 class NoteAdapter : RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
 
@@ -25,6 +29,26 @@ class NoteAdapter : RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
     }
 
     val differ = AsyncListDiffer(this, differCallback)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        return NoteViewHolder(
+            NoteLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+       return differ.currentList.size
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        val currentNode = differ.currentList[position]
+        holder.noteLayoutBinding.noteTitle.text = currentNode.noteTitle
+        holder.noteLayoutBinding.noteDesc.text = currentNode.noteDescription
+
+        holder.itemView.setOnClickListener {
+            val direction = HomeFragmentDirections.actionHomeFragmentToEditNoteFragment(currentNode)
+            it.findNavController().navigate(direction)
+        }
+    }
 
 
 }
