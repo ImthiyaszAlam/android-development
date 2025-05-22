a3ee91e689ed9ecc45d611cb7f1cc70303e93ac4
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-05-22 16:16:38 +0530

    added methods in repo from database using single expression methods

---
+++ MVVM-NotesApp/app/src/main/java/com/imthiyas/notes_app/repository/NoteRepository.kt
@@ -0,0 +1,19 @@
+package com.imthiyas.notes_app.repository
+
+import androidx.room.Query
+import com.imthiyas.notes_app.database.NoteDatabase
+import com.imthiyas.notes_app.model.Note
+
+class NoteRepository(private val noteDatabase: NoteDatabase) {
+
+    suspend fun addNote(note: Note) = noteDatabase.getNoteDao().addNotes(note)
+
+    suspend fun updateNote(note: Note) = noteDatabase.getNoteDao().updateNote(note)
+
+    suspend fun deleteNote(note: Note) = noteDatabase.getNoteDao().deleteNote(note)
+
+    fun getAllNotes() = noteDatabase.getNoteDao().getAllNotes()
+
+    fun searchNote(query: String) = noteDatabase.getNoteDao().searchNote(query)
+    
+}
