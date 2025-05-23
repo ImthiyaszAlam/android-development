43dc270be97000a991070143e6b83bc6c63d6d41
ImthiyaszAlam <developerimthiyas+imthiyas@gmail.com>
2025-05-23 12:59:06 +0530

    updateNote

--- MVVM-NotesApp/app/src/main/java/com/imthiyas/notes_app/ui/fragments/AddNoteFragment.kt
+++ MVVM-NotesApp/app/src/main/java/com/imthiyas/notes_app/ui/fragments/AddNoteFragment.kt
@@ -5,18 +5,47 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.lifecycle.Lifecycle
 import com.imthiyas.notes_app.R
+import com.imthiyas.notes_app.databinding.FragmentAddNoteBinding
+import com.imthiyas.notes_app.ui.MainActivity
+import com.imthiyas.notes_app.viewmodel.NoteViewModel
 
 
-class AddNoteFragment : Fragment() {
+class AddNoteFragment : Fragment(), MenuProvider {
+
+
+    private var fragmentAddNoteBinding: FragmentAddNoteBinding? = null
+    private val binding get() = fragmentAddNoteBinding!!
+    private lateinit var noteViewModel: NoteViewModel
+    private lateinit var addNoteView: View
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_note, container, false)
+        fragmentAddNoteBinding = FragmentAddNoteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
+        noteViewModel = (activity as MainActivity).noteViewModel
+        addNoteView = view
+
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        fragmentAddNoteBinding = null
     }
 
 }
